ccf1a63c8f21422e31e162562671c3f44f424fb1
sunminky <suns1502@gmail.com>
2021-11-19 23:44:28 +0900

    init

---
+++ src/main/java/MyChainCode.java
@@ -0,0 +1,97 @@
+import org.hyperledger.fabric.shim.ChaincodeBase;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class MyChainCode extends ChaincodeBase{
+    @Override
+    public Response init(ChaincodeStub stub) {
+        byte[] isAdminExist = stub.getState("admin");
+
+        if (isAdminExist.length == 0)
+            stub.putStringState("admin", "0");
+
+        return newSuccessResponse();
+    }
+
+    @Override
+    public Response invoke(ChaincodeStub stub) {
+        HashMap<String, Integer> methodDictionary = new HashMap<>();
+
+        methodDictionary.put("create_account", 0);
+        methodDictionary.put("remit", 1);
+        methodDictionary.put("balance_check", 2);
+        methodDictionary.put("money_issuance", 3);
+
+        switch (methodDictionary.get(stub.getFunction())){
+            // Account Opening
+            case 0:
+                return this.createAccount(stub);
+            // Remit
+            case 1:
+                return this.remit(stub);
+            // Balance Checking
+            case 2:
+                return this.balanceCheck(stub);
+            // Money Issuance
+            case 3:
+                return this.moneyIssuance(stub);
+        }
+
+        return newErrorResponse("Invoke Failed\n");
+    }
+
+    private Response createAccount(ChaincodeStub stub){
+        // Usage : create_account <user> <deposit>
+        List<String> args = stub.getParameters();
+        final int requiedArgsNum = 2;
+
+        return null;
+    }
+
+    private Response remit(ChaincodeStub stub){
+        // Usage : remit <user1> <user2> <amount>
+        List<String> args = stub.getParameters();
+        final int requiedArgsNum = 3;
+
+        return null;
+    }
+
+    private Response balanceCheck(ChaincodeStub stub){
+        // Usage : balance_check <user>
+        List<String> args = stub.getParameters();
+        final int requiedArgsNum = 1;
+
+        return null;
+    }
+
+    private Response moneyIssuance(ChaincodeStub stub){
+        // Usage : money_issuance <amount>
+        List<String> args = stub.getParameters();
+        final int requiedArgsNum = 1;
+
+        if(requiedArgsNum != args.size())
+            return newErrorResponse("[money issuance] Usage : money_issuance <amount>\n");
+
+        try {
+            int remain = Integer.parseInt(stub.getState("admin").toString());
+            int issuanceAmount = Integer.parseInt(args.get(0));
+
+            stub.putStringState("admin", new Integer(remain + issuanceAmount).toString());
+        }
+        catch (NumberFormatException e){
+            return newErrorResponse("[money issuance] invalid argument\n");
+        }
+        catch (Exception e){
+            return newErrorResponse("[money issuance] money issuance failed :(\n");
+        }
+
+        return newSuccessResponse("[money issuance] money issuance success~!\n");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello ChainCode\n");
+        System.out.println("This is Main\n");
+    }
+}
